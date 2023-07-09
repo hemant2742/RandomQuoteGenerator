324d4d3090e4f1afd446eca4be21c123dfb9b067
Hemant Kumar Singh <shemant820@gmail.com>
2023-07-09 21:09:25 +0530

    added react loader

--- src/components/cards.js
+++ src/components/cards.js
@@ -4,6 +4,7 @@ import Header from "./header";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Droppdown from "./dropdown";
+import { ThreeDots } from "react-loader-spinner";
 
 const storedBookmarks = localStorage.getItem("bookmarked");
 const bookmarked = storedBookmarks ? JSON.parse(storedBookmarks) : [];
@@ -13,6 +14,7 @@ function Card() {
   const [author, setAuthor] = useState("");
   const [setTags] = useState([]);
   const [selectedTag, setselectedTag] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   // http://api.quotable.io/random
 
@@ -25,6 +27,9 @@ function Card() {
       })
       .catch((err) => {
         console.log(err);
+      })
+      .finally(() => {
+        setIsLoading(false); // Set loading to false once the quote is fetched
       });
   }, []);
 
@@ -63,7 +68,6 @@ function Card() {
     );
   };
 
-  // console.log(selectedTagst);
   const handleBookmark = () => {
     bookmarked.push({ quote: quote, author: author });
     localStorage.setItem("bookmarked", JSON.stringify(bookmarked));
@@ -79,51 +83,60 @@ function Card() {
       theme: "dark",
     });
   };
+
   return (
     <>
       <Header />
-      <div className="px-5 py-28  flex h-screen  justify-center">
-        <div className=" flex flex-wrap     ">
-          <div className="p-4  md:w-full">
-            <div className="flex  rounded-3xl p-3 sm:flex-row flex-col bg-[#D05252]">
-              <div className="flex-grow text-center text-white  ">
-                <h2 className="text-lg title-font font-medium mb-6 text-center">
-                  {quote}
-                </h2>
-                <h3 className=" text-lg title-font font-small mb-0 ">
-                  -{author}
-                </h3>
-                <div className="float-right">
-                  <button onClick={handleBookmark}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="white"
-                      class="w-6 h-6 hover:fill-white"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
-                      />
-                    </svg>
-                  </button>
+      <div className="px-5 py-28 flex h-screen justify-center">
+        <div className="flex flex-wrap">
+          <div className="p-4 md:w-full">
+            {isLoading ? ( 
+              // Conditional rendering based on isLoading state
+              <div className="flex flex-col justify-center items-center ">
+                 <p className="text-3xl font-bold text-white mb-2 ">Getting Quote...</p>
+                <ThreeDots color="#D05252" height={80} width={80} />
+              </div>
+            ) : (
+              <div className="flex rounded-3xl p-3 sm:flex-row flex-col bg-[#D05252]">
+                <div className="flex-grow text-center text-white">
+                  <h2 className="text-lg title-font font-medium mb-6 text-center">
+                    {quote}
+                  </h2>
+                  <h3 className="text-lg title-font font-small mb-0">
+                    -{author}
+                  </h3>
+                  <div className="float-right">
+                    <button onClick={handleBookmark}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="white"
+                        className="w-6 h-6 hover:fill-white"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+                        />
+                      </svg>
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
+            )}
 
-            <div className=" flex flex-col items-center">
-              <div class="flex justify-center"></div>
-              <div className=" p-2">
+            <div className="flex flex-col items-center">
+              <div className="flex justify-center"></div>
+              <div className="p-2">
                 <Droppdown
                   selectedTag={selectedTag}
                   handleChange={handleChange}
                 />
               </div>
               <button
-                className="bg-green-500 hover:bg-green-800 Shadow-3xl text-white font-bold py-2 px-4  rounded-full  justify-center"
+                className="bg-green-500 hover:bg-green-800 Shadow-3xl text-white font-bold py-2 px-4 rounded-full justify-center"
                 onClick={fetchNewQuote}
               >
                 Next Quote
